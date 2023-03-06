cae929e3a6ae86de42204b0a5e542499827f3771
yipeng <sypsyp97@gmail.com>
2023-03-06 20:50:19 +0100

    1

--- reference.py
+++ reference.py
@@ -6,15 +6,15 @@ import numpy as np
 
 
 layer_type_space = ['conv_block', 'inverted_residual_block', 'mobilevit_block', 'None']
-kernel_size_space = [1, 3, 5, 7]
+kernel_size_space = [1, 2, 3, 4]
 stride_space = [1, 2]
-filters_space = [16, 24, 32, 48, 64, 96, 128, 192]
-expansion_factor_space = [2, 4]
+filters_space = [8, 12, 16, 24, 32, 48, 64, 96]
+expansion_factor_space = [1, 2]
 residual_space = ['None', 'Add', 'StochasticDepth', 'Concatenate']
 normalization_space = ['BatchNormalization', 'LayerNormalization']
 activation_space = ['relu', 'relu6', 'silu', 'silu6']
-transformer_space = [2, 3, 4, 5]
-head_space = [2, 4, 6, 8]
+transformer_space = [1, 2, 3, 4]
+head_space = [1, 2, 3, 4]
 
 
 def check_model(model):
