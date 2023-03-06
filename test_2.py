b841e1979e3786bee1759ea1f3f4a1873990f0d9
Yipeng <sypsyp97@gmail.com>
2023-03-06 19:13:26 +0100

    update

--- test_2.py
+++ test_2.py
@@ -11,48 +11,58 @@ import numpy as np
 from PIL import Image
 
 from pycoral.utils.edgetpu import make_interpreter
+import time
+import numpy as np
+from PIL import Image
+from pycoral.utils.edgetpu import make_interpreter
+from multiprocessing import Pool, cpu_count
 
 
-if __name__ == "__main__":
+def process_model(i):
+    try:
+        model_array = np.random.randint(0, 2, (9, 18))
+        model = create_model(model_array=model_array, num_classes=5, input_shape=(256, 256, 3))
+        while check_model(model):
+            model_array = np.random.randint(0, 2, (9, 18))
+            model = create_model(model_array=model_array, num_classes=5, input_shape=(256, 256, 3))
+
+        print(f"Generation:{i}")
+        inference_time = test_inference_time(model)
+        tflite_model_name = tflite_converter(model, i)
+        edgetpu_model_name = compile_edgetpu(tflite_model_name)
+
+        interpreter = make_interpreter(edgetpu_model_name)
+        interpreter.allocate_tensors()
+
+        input_details = interpreter.get_input_details()[0]
+        output_details = interpreter.get_output_details()[0]
+
+        image_file = 'test.jpg'
+        image = Image.open(image_file).convert('RGB')
+        image = np.array(image.resize((input_details['shape'][1], input_details['shape'][2])))
+
+        input_tensor = np.expand_dims(image, axis=0).astype(input_details['dtype'])
+        interpreter.set_tensor(input_details['index'], input_tensor)
+
+        start_time = time.monotonic()
+        interpreter.invoke()
+        tpu_inference_time = (time.monotonic() - start_time) * 1000
+        return (inference_time, tpu_inference_time)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
     inference_times = []
     tpu_inference_times = []
 
-    for i in range(500):
-        try:
-            model_array = np.random.randint(0, 2, (9, 18))
-            model = create_model(model_array=model_array, num_classes=5, input_shape=(256, 256, 3))
-            while check_model(model):
-                model_array = np.random.randint(0, 2, (9, 18))
-                model = create_model(model_array=model_array, num_classes=5, input_shape=(256, 256, 3))
+    with Pool(cpu_count()) as p:
+        results = p.map(process_model, range(500))
 
-            print(f"Generation:{i}")
-            inference_time = test_inference_time(model)
-            inference_times.append(inference_time)
-            tflite_model_name = tflite_converter(model, i)
-            edgetpu_model_name = compile_edgetpu(tflite_model_name)
-
-            interpreter = make_interpreter(edgetpu_model_name)
-            interpreter.allocate_tensors()
-
-            input_details = interpreter.get_input_details()[0]
-            output_details = interpreter.get_output_details()[0]
-
-            image_file = 'test.jpg'
-            image = Image.open(image_file).convert('RGB')
-            image = np.array(image.resize((input_details['shape'][1], input_details['shape'][2])))
-
-            input_tensor = np.expand_dims(image, axis=0).astype(input_details['dtype'])
-            interpreter.set_tensor(input_details['index'], input_tensor)
-
-            start_time = time.monotonic()
-            interpreter.invoke()
-            tpu_inference_time = (time.monotonic() - start_time) * 1000
-            tpu_inference_times.append(tpu_inference_time)
-        except Exception as e:
-            print(e)
-
-    print(inference_times)
-    print(tpu_inference_times)
+    for r in results:
+        if r is not None:
+            inference_times.append(r[0])
+            tpu_inference_times.append(r[1])
 
     with open('inference_times.pkl', 'wb') as f:
         pickle.dump(inference_times, f)
@@ -75,16 +85,3 @@ if __name__ == "__main__":
 
     # Displaying the plot
     plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
