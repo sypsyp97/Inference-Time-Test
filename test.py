3a264d5db373e797b2f3dfc4372dd759f81cfd13
yipeng <sypsyp97@gmail.com>
2023-03-06 19:36:40 +0100

    1

--- test.py
+++ test.py
@@ -25,9 +25,6 @@ if __name__ == "__main__":
                 model_array = np.random.randint(0, 2, (9, 18))
                 model = create_model(model_array=model_array, num_classes=5, input_shape=(256, 256, 3))
 
-            print(f"Generation:{i}")
-            inference_time = test_inference_time(model)
-            inference_times.append(inference_time)
             tflite_model_name = tflite_converter(model, i)
             edgetpu_model_name = compile_edgetpu(tflite_model_name)
 
@@ -48,6 +45,9 @@ if __name__ == "__main__":
             interpreter.invoke()
             tpu_inference_time = (time.monotonic() - start_time) * 1000
             tpu_inference_times.append(tpu_inference_time)
+            inference_time = test_inference_time(model)
+            inference_times.append(inference_time)
+            print(f"Model:{i}")
             print(inference_time, tpu_inference_time)
         except Exception as e:
             print(e)
