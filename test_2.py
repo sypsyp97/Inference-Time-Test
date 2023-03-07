c8e68817f30a07182a175d1662ff0c6abc5e998e
yipeng <sypsyp97@gmail.com>
2023-03-07 12:58:35 +0100

    1

--- test_2.py
+++ test_2.py
@@ -3,15 +3,18 @@ from reference import check_model
 from raw_inference_time import test_inference_time
 from TFLite_Converter import tflite_converter
 from Compile_Edge_TPU import compile_edgetpu
+import pickle
+import matplotlib.pyplot as plt
 
 import time
 import numpy as np
 from PIL import Image
+
 from pycoral.utils.edgetpu import make_interpreter
 from multiprocessing import Pool, cpu_count
 
 
-def create_model_and_test_inference_time(i):
+def inference(model_index):
     try:
         model_array = np.random.randint(0, 2, (9, 18))
         model = create_model(model_array=model_array, num_classes=5, input_shape=(256, 256, 3))
@@ -19,7 +22,7 @@ def create_model_and_test_inference_time(i):
             model_array = np.random.randint(0, 2, (9, 18))
             model = create_model(model_array=model_array, num_classes=5, input_shape=(256, 256, 3))
 
-        tflite_model_name = tflite_converter(model, i)
+        tflite_model_name = tflite_converter(model, model_index)
         edgetpu_model_name = compile_edgetpu(tflite_model_name)
 
         interpreter = make_interpreter(edgetpu_model_name)
@@ -37,27 +40,52 @@ def create_model_and_test_inference_time(i):
         start_time = time.monotonic()
         interpreter.invoke()
         tpu_inference_time = (time.monotonic() - start_time) * 1000
+
         inference_time = test_inference_time(model)
-        print(f"Model:{i}")
+
+        print(f"Model:{model_index}")
         print(inference_time, tpu_inference_time)
 
         return inference_time, tpu_inference_time
     except Exception as e:
         print(e)
+        return None
+    finally:
+        del model
+        del interpreter
+        del input_tensor
 
 
 if __name__ == "__main__":
+    pool = Pool(processes=8)
+
     num_models = 500
-    inference_times = []
-    tpu_inference_times = []
+    results = pool.map(inference, range(num_models))
+    inference_times, tpu_inference_times = zip(*results)
+    inference_times = [x for x in inference_times if x is not None]
+    tpu_inference_times = [x for x in tpu_inference_times if x is not None]
 
-    with Pool(processes=min(cpu_count() - 1, 10)) as p:
-        results = p.map(create_model_and_test_inference_time, range(num_models))
+    print(inference_times)
+    print(tpu_inference_times)
 
-    for r in results:
-        if r is not None:
-            inference_times.append(r[0])
-            tpu_inference_times.append(r[1])
+    with open('inference_times.pkl', 'wb') as f:
+        pickle.dump(inference_times, f)
 
-    print("Inference times:", inference_times)
-    print("TPU inference times:", tpu_inference_times)
+    with open('tpu_inference_times.pkl', 'wb') as f:
+        pickle.dump(tpu_inference_times, f)
+
+    with open('inference_times.pkl', 'rb') as f:
+        inference_times = pickle.load(f)
+
+    with open('tpu_inference_times.pkl', 'rb') as f:
+        tpu_inference_times = pickle.load(f)
+
+    plt.plot(inference_times, tpu_inference_times)
+
+    # Adding title and labels to the plot
+    plt.title('Inference Times vs TPU Inference Times')
+    plt.xlabel('Inference Time (seconds)')
+    plt.ylabel('TPU Inference Time (seconds)')
+
+    # Displaying the plot
+    plt.show()
