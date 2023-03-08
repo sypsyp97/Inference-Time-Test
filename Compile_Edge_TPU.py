62d472f3b7d967b60f3f9cf20228f20340854a82
yipeng <sypsyp97@gmail.com>
2023-03-08 17:02:00 +0100

    1

--- Compile_Edge_TPU.py
+++ Compile_Edge_TPU.py
@@ -6,8 +6,11 @@ def compile_edgetpu(tflite_model_name):
         print(f"{tflite_model_name} not found")
         return
 
+    output_folder = "models"
     edgetpu_model_name = tflite_model_name.replace('.tflite', '_edgetpu.tflite')
-    os.system('edgetpu_compiler -sa {}'.format(tflite_model_name))
+
+    os.system('edgetpu_compiler -sa {} -o {}'.format(tflite_model_name, output_folder))
+
+    # os.system('edgetpu_compiler -sa {}'.format(tflite_model_name))
 
     return edgetpu_model_name
-
