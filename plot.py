2b04e7dd2cb4bbd6a8f39867b3e0deb71135b95d
yipeng <sypsyp97@gmail.com>
2023-03-08 14:47:46 +0100

    1

--- plot.py
+++ plot.py
@@ -12,8 +12,8 @@ with plt.style.context('seaborn-whitegrid'):
     fig, ax = plt.subplots()
     ax.scatter(inference_times, tpu_inference_times, alpha=0.5)
     ax.set_title('Inference Times vs TPU Inference Times')
-    ax.set_xlabel('Inference Time (seconds)')
-    ax.set_ylabel('TPU Inference Time (seconds)')
+    ax.set_xlabel('Inference Time (ms)')
+    ax.set_ylabel('TPU Inference Time (ms)')
 
     # Displaying the plot
     plt.show()
